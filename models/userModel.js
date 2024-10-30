f1082a1d1fc4603165993fccfd3d6d48c1cc773b
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-10-30 13:57:50 +0000

    fixed merge issue

--- models/userModel.js
+++ models/userModel.js
@@ -5,11 +5,16 @@ const createUser = async (username, email, hashedPassword) => {
     .from("users")
     .insert([{ username, email, password: hashedPassword }]);
 
+  if (error) throw new Error(error.message);
+  return data[0];
+};
+
 const loginUser = async (email, password) => {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
+
   if (error) throw new Error("Login failed: " + error.message);
   return data;
 };
